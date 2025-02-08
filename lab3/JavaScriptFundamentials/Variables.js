4e4b20e54f13fccd79cee82588a2f620af3aa334
Balym <b_sekenova@kbtu.kz>
2025-02-08 16:23:01 +0500

    Lab3 JavaScript Fundamentals done

---
+++ lab3/JavaScriptFundamentials/Variables.js
@@ -0,0 +1,13 @@
+//task 1
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
+
+//task 2
+let planet = "Earth";
+let visitorName = "John";
+
+//task 3
+const birthday = '18.04.1982';
+const age = someCode(birthday);
