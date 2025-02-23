dbb82298ac33c85ecfb4cdf8d7fab283fac93fa1
Balym <b_sekenova@kbtu.kz>
2025-02-23 15:12:55 +0500

    reorganizing+lab5 done

---
+++ lab5/app/src/app/productCategories.ts
@@ -0,0 +1,26 @@
+export interface ProductCategory {
+    name: string;
+    img: string;
+
+  }
+  
+  export const productCategories = [
+    {
+     name: 'Когтеточки',
+     img: 'https://resources.cdn-kaspi.kz/img/m/p/hb9/h73/86349420560414.jpg?format=gallery-large'
+
+    },
+    {
+      name: 'Игровые комплексы',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/p9e/p1d/23296812.jpeg?format=gallery-large'
+    },
+    {
+      name: 'Лежанки',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/p28/p07/3788046.jpeg?format=gallery-large'
+    },
+    {
+      name: 'Корм',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/p05/p11/5058379.jpg?format=gallery-large'
+    },
+    
+  ];
